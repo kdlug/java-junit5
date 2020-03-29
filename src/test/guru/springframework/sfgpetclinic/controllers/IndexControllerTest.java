4e4b51587854ccfc2ffe377e7d43003d03ddb55c
kdlug <casi@o2.pl>
2020-03-29 16:25:09 +0200

    Test interfaces

--- src/test/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.ControllerTests;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
 
@@ -12,8 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-@Tag("controller")
-class IndexControllerTest {
+class IndexControllerTest implements ControllerTests {
 
     IndexController controller;
 
