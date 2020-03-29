1775ead6f9752bf9362b81ead912c6f66cc0af45
kdlug <casi@o2.pl>
2020-03-29 19:18:03 +0200

    Junit parametrized value tests

--- src/test/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -25,5 +28,12 @@ class OwnerTest implements ModelTests {
 
         // hamecrest
         assertThat(owner.getCity(), is("Key West"));
+    }
+
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - #{index} {arguments}")
+    @ValueSource(strings = {"Hello", "World", "Everyone"})
+    void testValueSource(String val) {
+        System.out.println(val);
     }
 }
